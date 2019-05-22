1917a51d8f521442e97b49c0d36eb0df9e5b42d9
周诚 <chengzcom0926@gmail.com>
2019-05-22 16:30:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import os
-
+import time
 
 
 print(os.getcwd())
 
 print(os.listdir("/home"))
 print(os.listdir(os.getcwd()))
+
+
+f=os.open("ddd/bbb")
+
+f.write(time.time())
+f.write("\n")
+f.close()
+
+
+print(os.open("ddd/bbb").read())
