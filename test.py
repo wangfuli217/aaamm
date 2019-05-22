0749154caf5f17493640fca29f8820ca20a13e14
周诚 <chengzcom0926@gmail.com>
2019-05-22 16:31:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ print(os.listdir("/home"))
 print(os.listdir(os.getcwd()))
 
 
-f=os.open("ddd/bbb")
+f=os.open("ddd/bbb","aw")
 
 f.write(time.time())
 f.write("\n")
 f.close()
 
 
-print(os.open("ddd/bbb").read())
+print(os.open("ddd/bbb","r").read())
